347c11c6d733580978005ec77ab82638c00fda72
Max-Mendes91 <maxmendesnoah1991@gmail.com>
2025-10-09 22:07:57 +0200

    Add designer attribute and version number to logo in index.html; refactor event listener syntax in script.js and add new functionality for scrolling and event handling.

--- starter/script.js
+++ starter/script.js
@@ -19,7 +19,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-btnsOpenModal.forEach(btn =>btn.addEventListener('click', openModal));
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 
 for (let i = 0; i < btnsOpenModal.length; i++)
@@ -34,7 +34,7 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-
+/*
 //SELECING ELEMENTS 
 console.log(document.documentElement);
 console.log(document.head);
@@ -73,4 +73,98 @@ header.append(message);
 document.querySelector('.btn--close-cookie').addEventListener('click', () => {
   message.remove();
 });
+
+//Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height = Number.parseFloat(getComputedStyle(message).height) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+//ATRIBUTES
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+
+//NON-STANDART ATRIBUTE
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist')
+console.log(logo.getAttribute('src'));
+
+
+const link = document.querySelector('.nav__link--btn')
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+
+//DATA ATRIBUTES
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c', 'i')
+logo.classList.remove('c', 'i')
+logo.classList.toggle('c')
+logo.classList.contains('c')
+
+//DONT USE
+logo.clasName = 'jonas'
+*/
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', (e) => {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+  console.log('height/width viewport', document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  //Scrolling
+  // window.scrollTo(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset)
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset, 
+  //   top: s1coords.top + window.pageYOffset, 
+  //   behavior: "smooth",
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+//EVENT LISTENER AND EVENT HANDLERS
+
+const h1 = document.querySelector('h1');
+const alertH1 = (e) => {
+  alert('addEventListener: great! you are reading the heading');
+  
+  
+};
+
+h1.addEventListener('mouseenter', alertH1);
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// h1.onmouseenter = (e) => alert('addEventListener: great! you are reading the heading')
+
+
+
+
+
+
+
+
+
 
